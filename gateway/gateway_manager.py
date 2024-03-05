9ef57eb8ff689e18244ff2ea90ccd0cd7026a8ba
JOCAO24 <jcaicedo50@gmail.com>
2024-03-04 20:35:14 -0500

    Added interactive menu and log management.

--- gateway/gateway_manager.py
+++ gateway/gateway_manager.py
@@ -1,7 +1,9 @@
+import sys
 import threading
 import time
 import Pyro4
 from gateway.class_for_gateway.manage_data_gateway import ManageDataNameServer
+from manage_logs.manage_logs import log_message, get_end_session_log
 from security.authenticate import AuthenticationManager
 from utils.errors import ErrorTypes
 from utils.types.agent_nameserver_type import AgentNSType
@@ -18,7 +20,9 @@ port_nameserver = 9090
 
 
 class GatewayManager:
-    def __init__(self):
+    def __init__(self, event, finally_name_server):
+        self.event = event
+        self.finally_name_server = finally_name_server
         self.nameserver = Pyro4.locateNS(host=local_ip, port=port_nameserver)
         self.server_uri = None
         self.server = None
@@ -34,11 +38,13 @@ class GatewayManager:
                 is_valid_ip = validate_ip(ip_yp)
                 if not is_valid_ip:
                     print("The IP entered is not valid. Please enter a valid IP.")
+                    log_message("The IP entered is not valid. Please enter a valid IP.")
                 else:
                     request_ip = False
             else:
                 ip_yp = get_ip()
                 request_ip = False
+            log_message(f"The IP of the yellow_page is: {ip_yp}")
         return ip_yp
 
     def check_server(self):
@@ -52,17 +58,24 @@ class GatewayManager:
                 if ip_yp:
                     is_valid_ip = validate_ip(ip_yp)
                 if not is_valid_ip or not ip_yp:
-                    print("The IP of the yp saved in the configuration file is not valid. Please enter a valid IP.")
+                    message = "The IP of the yp saved in the configuration file is not valid. Please enter a valid IP."
+                    print(message)
+                    log_message(message)
                     ip_yp = self.request_ip()
                 break
             elif opt_select.lower() == 'n':
                 ip_yp = self.request_ip()
                 break
             else:
-                print("Invalid option. Please enter a valid option.")
+                message = "Invalid option. Please enter a valid option."
+                print(message)
+                log_message(message)
 
         if ip_yp:
             ManageDataNameServer().save_config(ip_yp)
+            message = f"Wating for the yellow_page to be registered at {ip_yp}..."
+            print(message)
+            log_message(message)
             while self.server_uri is None:
                 try:
                     name_yellow_page = 'yellow_page@' + ip_yp
@@ -70,14 +83,19 @@ class GatewayManager:
                     if self.server_uri:
                         self.server = Pyro4.Proxy(self.server_uri)
                         ip = self.server_uri.location
-                        print("The yellow_page has been registered! Your ip is:", ip)
+                        # print("The yellow_page has been registered! Your ip is:", ip)
+                        message = f"The yellow_page has been registered! Your ip is: {ip}"
+                        log_message(message)
+                        print(message)
+                        self.event.set()
+                        time.sleep(1)
                     else:
-                        print("Waiting for the yellow_page to be registered...")
-                        time.sleep(2)
+                        time.sleep(1)
 
                 except Pyro4.errors.NamingError:
-                    print("Waiting for the yellow_page to be registered...")
-                    time.sleep(2)
+                    time.sleep(1)
+
+        self.manage_gateway()
 
     def get_ip_entinty(self):
         return Pyro4.current_context.client_sock_addr[0]
@@ -94,12 +112,15 @@ class GatewayManager:
         ip_entity = self.get_ip_entinty()
         try:
             is_authenticated = self.security_manager.authenticate(ip_entity, client_data["code_otp"])
-            print(f"Client registration request {client_data['name']} was {is_authenticated}.")
+            # print(f"Client registration request {client_data['name']} was {is_authenticated}.")
             if is_authenticated is True:
                 return True
             else:
-                raise ValueError("The client could not be registered.")
+                message = "Warning: The client could not be registered."
+                log_message(message)
+                raise ValueError(message)
         except CustomException as e:
+            log_message(f"Error: {e.error_type.message}")
             return {"error": e.error_type.code, "message": e.error_type.message}
 
     @Pyro4.expose
@@ -107,8 +128,10 @@ class GatewayManager:
         ip_entity = self.get_ip_entinty()
         try:
             code_otp = agent_data.get("code_otp", None)
+            log_message(f"Agent registration request {agent_data['name']} with code_otp {code_otp}.")
             is_authenticated = self.security_manager.authenticate(ip_entity, code_otp)
-            print(f"Agent registration request {agent_data['name']} was {is_authenticated}.")
+            # print(f"Agent registration request {agent_data['name']} was {is_authenticated}.")
+            log_message(f"Agent registration request {agent_data['name']} was {is_authenticated}.")
             if is_authenticated is True:
                 self.server.register_agent({
                     "name": agent_data["name"],
@@ -119,7 +142,9 @@ class GatewayManager:
                 })
                 return True
             else:
-                raise ValueError("The agent could not be registered.")
+                message = "Warning: The agent could not be registered."
+                log_message(message)
+                raise ValueError(message)
         except CustomException as e:
             # Si la ip ha sido bloqueada (raise CustomException(ErrorTypes.ip_blocked)) se le notifica al yellow_page
             # para que elimine los agentes registrados con esa ip.
@@ -129,13 +154,42 @@ class GatewayManager:
 
     @Pyro4.expose
     def execute_operation(self, skill, num1, num2):
-        print(f"Operation {skill} with numbers {num1} and {num2} requested.")
+        log_message(f"Operation {skill} with numbers {num1} and {num2} requested.")
         if not self.server:
+            message = f"The yellow_page is not registered or is not active."
+            log_message(message)
             raise Pyro4.errors.CommunicationError("The yellow_page is not registered or is not active.")
         return self.server.execute_operation(skill, num1, num2)
 
     @Pyro4.expose
     def get_skills(self):
         if not self.server:
+            message = f"The yellow_page is not registered or is not active."
+            log_message(message)
             raise Pyro4.errors.CommunicationError("The yellow_page is not registered or is not active.")
         return self.server.get_skills()
+
+    def manage_gateway(self):
+        while True:
+            logs = get_end_session_log()
+            print("1. View logs")
+            print("2. Add whitelist")
+            print("3. Add blacklist")
+            print("4. View whitelist")
+            print("5. View blacklist")
+            print("6. Exit")
+            option = input("Enter the number of the option you want to execute: ")
+            if option.isdigit():
+                option = int(option)
+                if option == 1:
+                    print("====================================================================")
+                    for log in logs:
+                        print(log)
+                    print("====================================================================")
+                elif option == 6:
+                    self.finally_name_server.set()
+                    break
+                else:
+                    print("Invalid option. Please enter a valid option.")
+            else:
+                print("Invalid option. Please enter a valid option.")
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+import signal
 import subprocess
+import sys
+import threading
 import time
 import Pyro4
 from gateway.gateway_manager import GatewayManager
 from controller.nameserver_controller import NameServerController
+from manage_logs.manage_logs import start_new_session_log, log_message
 from utils.custom_exception import CustomException
 from utils.pyro_config import configure_pyro
 from utils.get_ip import get_ip
+import datetime
 
 configure_pyro()
 ip_local = get_ip()
 port_nameserver = 9090
+global_logs = []
+
+
+def signal_handler(signum, frame):
+    log_message("Cierre intencional por el usuario")
+    sys.exit(0)
+
+
+def setup_signal_handlers():
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
 def create_nameserver_space():
     cmd = f"pyro4-ns --host {ip_local} -p 9090"
     subprocess.Popen(cmd, shell=True)
-    print(f"Name Server started in: {ip_local}:9090")
+    # print(f"Name Server started in: {ip_local}:9090")
+    start_new_session_log()
+    global_logs.append(log_message(f"Name Server started in: {ip_local}:9090"))
 
 
 def wait_for_nameserver():
@@ -28,25 +46,54 @@ def wait_for_nameserver():
     return False
 
 
-if __name__ == '__main__':
-    Pyro4.util.SerializerBase.register_class_to_dict(CustomException, CustomException.__dict__)
-    create_nameserver_space()
+def check_finally_event():
+    finally_name_server.wait()
+    log_message("Finally. Shutting down...")
+    daemon.shutdown()
+    sys.exit(0)
 
-    if not wait_for_nameserver():
-        print("Error: NameServer did not start correctly.")
-        exit(1)
 
-    daemon = Pyro4.Daemon(host=ip_local, port=9091)
-    nameserver = Pyro4.locateNS(host=ip_local, port=port_nameserver)
-    print(f"NameServer located: {nameserver}")
-
-    gateway = GatewayManager()
-    uri_gateway = daemon.register(gateway)
-    print(f"GatewayManager registered with URI: {uri_gateway}")
-    nameserver.register("gateway_manager", uri_gateway)
-
-    ns_controller = NameServerController(nameserver, gateway, gateway_uri=uri_gateway)
-    uri_ns_controller = daemon.register(ns_controller)
-    nameserver.register("ns_controller", uri_ns_controller)
-
+def daemon_loop():
     daemon.requestLoop()
+
+
+if __name__ == '__main__':
+    try:
+        yp_located_event = threading.Event()
+        finally_name_server = threading.Event()
+        Pyro4.util.SerializerBase.register_class_to_dict(CustomException, CustomException.__dict__)
+        create_nameserver_space()
+
+        if not wait_for_nameserver():
+            # print("Error: NameServer did not start correctly.")
+            global_logs.append(log_message("Error: NameServer did not start correctly."))
+            exit(1)
+
+        daemon = Pyro4.Daemon(host=ip_local, port=9091)
+        nameserver = Pyro4.locateNS(host=ip_local, port=port_nameserver)
+        # print(f"NameServer located: {nameserver}")
+        global_logs.append(log_message(f"NameServer located: {nameserver}"))
+
+        gateway_manager = GatewayManager(yp_located_event, finally_name_server)
+        uri_gateway = daemon.register(gateway_manager)
+        # print(f"GatewayManager registered with URI: {uri_gateway}")
+        global_logs.append(log_message(f"GatewayManager registered with URI: {uri_gateway}"))
+        nameserver.register("gateway_manager", uri_gateway)
+        yp_located_event.wait()
+        ns_controller = NameServerController(nameserver, gateway_manager, gateway_uri=uri_gateway)
+        uri_ns_controller = daemon.register(ns_controller)
+        nameserver.register("ns_controller", uri_ns_controller)
+        # print(f"NameServerController registered with URI: {uri_ns_controller}")
+        global_logs.append(log_message(f"NameServerController registered with URI: {uri_ns_controller}"))
+
+        print("NameServer running...")
+        log_message("NameServer running...")
+
+        daemon_thread = threading.Thread(target=daemon_loop)
+        daemon_thread.start()
+
+        check_finally_thread = threading.Thread(target=check_finally_event)
+        check_finally_thread.start()
+    except Exception as e:
+        print(e)
+        log_message(f"Error: {e}")
