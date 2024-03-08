9fef6cb3f51ad5caff5221aae3cec68b84bdfdd3
olivaresdei6 <olivaresdei6@gmail.com>
2024-03-08 10:03:46 -0500

    The path to the directory where the ns data is stored has been corrected

--- gateway/class_for_gateway/manage_data_gateway.py
+++ gateway/class_for_gateway/manage_data_gateway.py
@@ -5,7 +5,7 @@ from Pyro4.util import json
 class ManageDataNameServer:
     def save_config(self, ip_yellow_page: str):
         if not os.path.exists('data'):
-            os.makedirs('nameserver/data')
+            os.makedirs('data')
         with open(f'data/config.json', 'w') as file:
             json.dump({"ip_yellow_page": ip_yellow_page}, file)
 
